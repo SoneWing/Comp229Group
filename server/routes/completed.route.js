1229847a467c450554a1ed7d39b640fbdc527c9d
Junshen Weng <sonewing1997@gmail.com>
2023-11-20 17:18:26 -0500

    full structure commit

---
+++ server/routes/completed.route.js
@@ -0,0 +1,8 @@
+import express from "express";
+import completedCtrl from "../controllers/completed.controller";
+import surveyCtrl from "../controllers/survey.controller";
+import authCtrl from "../controllers/auth.controller";
+import surveySiteCtrl from "../controllers/surveysite.controller";
+import userCtrl from "../controllers/user.controller"
+
+const router = express.Router();
